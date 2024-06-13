01c1239af56ed084993e4e822cf5bfb33df21638
DiogoBrito05 <britu.diogo@gmail.com>
2024-06-13 15:47:36 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,42 @@
+import os
+
+def exibir_nome_do_programa():
+    print("""     
+    ░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
+    ██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗  ██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝
+    ╚█████╗░███████║██████╦╝██║░░██║██████╔╝  █████╗░░░╚███╔╝░██████╔╝██████╔╝█████╗░░╚█████╗░╚█████╗░
+    ░╚═══██╗██╔══██║██╔══██╗██║░░██║██╔══██╗  ██╔══╝░░░██╔██╗░██╔═══╝░██╔══██╗██╔══╝░░░╚═══██╗░╚═══██╗
+    ██████╔╝██║░░██║██████╦╝╚█████╔╝██║░░██║  ███████╗██╔╝╚██╗██║░░░░░██║░░██║███████╗██████╔╝██████╔╝
+    ╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░
+    """)
+
+def exibir_opcaoes():
+    print('1. Cadastrar Restaurante')
+    print('2. Listar Restaurante')
+    print('3. Ativar Restaurante')
+    print('4. Sair')
+
+def finalizar_app():
+    os.system('cls')
+    print('Finalizando o app\n')     
+    
+def escolher_opcao():    
+    opcao_escolhida = int (input('Escolha uma opção: '))
+    
+    if opcao_escolhida == 1 :
+        print ('Cadastrar restaurante')
+    elif opcao_escolhida == 2 :
+        print ('Listar restaurante')
+    elif opcao_escolhida == 3:
+        print('Ativar restaurante')
+    else: 
+        finalizar_app()
+    
+def main():
+    exibir_nome_do_programa()
+    exibir_opcaoes()
+    escolher_opcao()
+        
+if __name__ == '__main__' :
+    main()        
+# teste12
